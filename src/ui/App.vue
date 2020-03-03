f3121ff4e05647a2ae567a8e268dff51321cc43f
Jamie Carr <jamie.carr.07@gmail.com>
2020-03-03 23:56:11 +0000

    added some example ds views

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <Welcome />
+  </div>
 </template>
 
 <script>
+import "../figma/figma-ds/js/selectMenu";
+import "../figma/figma-ds/js/iconInput";
+import "../figma/figma-ds/js/disclosure";
+
+import Welcome from "./components/Welcome";
+
 export default {
   data() {
     return {};
+  },
+
+  components: {
+    Welcome
+  },
+
+  mounted() {
+    // Initialize the figma-ds components
+    window.selectMenu.init();
+    window.iconInput.init();
+    window.disclosure.init();
   }
 };
 </script>
 
 <style lang="scss">
+@import "../figma/figma-ds/figma-plugin-ds";
 </style>
